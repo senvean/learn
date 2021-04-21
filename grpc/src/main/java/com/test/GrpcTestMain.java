ce18aae6a11c607e2c347e3dc25896b876e4d3c7
sunw507 <sunw507@sany.com.cn>
2021-04-21 16:47:21 +0800

    init

---
+++ grpc/src/main/java/com/test/GrpcTestMain.java
@@ -0,0 +1,16 @@
+package com.test;
+
+/**
+ * 说明：
+ *
+ * @Title : GrpcTestMain.java
+ * @Package com.test
+ * @See: {@link GrpcTestMain}<br/>
+ * Copyright: Copyright (c) 2017<br/>
+ * Company:sany huax witsight team by product
+ * @Author : sunw507
+ * @Date: 2021-04-15 13:37:22
+ * @version: V1.0
+ */
+public class GrpcTestMain {
+}
